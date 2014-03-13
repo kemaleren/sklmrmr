6c564dc7cc91efe9e5daa31d78bef355f6319354
Kemal Eren <kemal@kemaleren.com>
2014-03-12 20:15:48 -0700

    slight refactoring of MRMR class

--- sklmrmr/__init__.py
+++ sklmrmr/__init__.py
@@ -6,52 +6,48 @@ __all__ = ['MRMR']
 
 
 import numpy as np
-from sklearn.base import BaseEstimator, clone
+from sklearn.base import BaseEstimator
 from sklearn.feature_selection.base import SelectorMixin
-from sklearn.utils import check_arrays, safe_mask
+from sklearn.utils import check_arrays
 from ._mrmr import _mrmr, MAXREL, MID, MIQ
 
 
 class MRMR(BaseEstimator, SelectorMixin):
+    methods = {'maxrel': MAXREL, 'mid': MID, 'miq': MIQ}
 
-    def __init__(self, k=None, method='MID', normalize=False, similar=False, verbose=0):
-
-        method = method.upper()
-        if method not in ('MAXREL', 'MID', 'MIQ'):
-            raise ValueError("method must be one of 'MAXREL', 'MID', or 'MIQ'")
-
+    def __init__(self, k=None, method='mid', normalize=False,
+                 verbose=0):
+        self.method = method
         self.k = k
         self.method = method
         self.normalize = normalize
-        self.similar = similar
         self.verbose = verbose
 
+    def _validate(self):
+        if self.method not in self.methods:
+            raise ValueError("Unknown method: {}. Method must be one"
+                             " of {}".format(self.method, self.methods.keys()))
+
     def fit(self, X, y):
+        self._validate()
         X, y = check_arrays(X, y, sparse_format="csc")
         n_samples, n_features = X.shape
+
+        # TODO: ensure only discrete features
 
         if self.k is None:
             k = n_features // 2
         else:
             k = self.k
 
-        if self.method == 'MAXREL':
-            method = MAXREL
-        elif self.method == 'MID':
-            method = MID
-        elif self.method == 'MIQ':
-            method = MIQ
-        else:
-            raise RuntimeError("unknown method: '{0}'".format(method))
-
         X_classes = np.array(list(set(X.reshape((n_samples * n_features,)))))
         y_classes = np.array(list(set(y.reshape((n_samples,)))))
 
-        idxs, _ = _mrmr(n_samples, n_features,
-                y.astype(np.long), X.astype(np.long),
-                y_classes.astype(np.long), X_classes.astype(np.long),
-                y_classes.shape[0], X_classes.shape[0],
-                k, method, self.normalize)
+        method = self.methods[self.method]
+        idxs, _ = _mrmr(n_samples, n_features, y.astype(np.long),
+                        X.astype(np.long), y_classes.astype(np.long),
+                        X_classes.astype(np.long), y_classes.shape[0],
+                        X_classes.shape[0], k, method, self.normalize)
 
         support_ = np.zeros(n_features, dtype=np.bool)
         ranking_ = np.ones(n_features, dtype=np.int) + k
